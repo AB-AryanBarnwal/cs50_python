2521fa1d568eea633bb7d1236194a5c6a1babe73
AB-AryanBarnwal <ab.aryanbarnwal@gmail.com>
2025-11-20 17:01:30 +0530

    tidying week4

---
+++ notes/week4/week4_module.py
@@ -0,0 +1,13 @@
+from artists import get_artworks
+
+
+def main():
+    print("Search the Art Institue of Chicago")
+    artwork = input("Arwork:")
+    artworks= get_artworks(query = artwork, limit = 3)
+    for artwork in artworks:
+            print(f"* {artwork}")
+
+
+            
+main()
