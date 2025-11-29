ebde97096aeb4e7838ecd5cb79f213fd9e20ca51
AB-AryanBarnwal <ab.aryanbarnwal@gmail.com>
2025-11-29 11:45:44 +0530

    week5

--- notes/week5/test.py
+++ notes/week5/test.py
@@ -1,1 +1,26 @@
+import pytest
+from cal import square
 
+def main():
+    test_pos()
+    test_neg()
+    test_zero()
+    test_str()
+
+def test_pos():
+    assert square(2) == 4
+    assert square(3) == 9
+
+def test_neg():
+    assert square(-2) == 4
+    assert square(-3) == 9
+
+def test_zero():
+    assert square(0) == 0
+
+def test_str():
+    with pytest.raises(TypeError):
+        square("cat")
+
+if __name__ == "__main__":
+    main()
