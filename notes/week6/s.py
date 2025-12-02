a5893e8e6c2f2c91b690b6b9bc979737a1913f5e
AB-AryanBarnwal <ab.aryanbarnwal@gmail.com>
2025-12-02 17:30:53 +0530

    week6

--- notes/week6/s.py
+++ notes/week6/s.py
@@ -3,10 +3,8 @@ pokemon_list = []
 with open("s.csv") as file:
     for line in file:
         pok, type = line.rstrip().split(",")
-        pokemon={}
-        pokemon["pok"] = pok
-        pokemon["type"] = type
+        pokemon={"pok": pok, "type": type}
         pokemon_list.append(pokemon)
 
-for p in pokemon_list:
+for p in sorted(pokemon_list):
     print(f"{p['pok']} is in {p['type']}")
